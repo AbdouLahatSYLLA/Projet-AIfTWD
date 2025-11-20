8b852a6de9e6ab571e05bb0d7cadcbc567fb975d
Abdou-SYLLA <syllaa@3il.fr>
2025-11-20 02:15:08 +0200

    add first preprocess data result

--- Partie1/code/AIfTwD_project.py
+++ Partie1/code/AIfTwD_project.py
@@ -9,9 +9,10 @@ import seaborn as sns
 
 
 
-# Nettoyage et Prétraitement (Preprocessing)
+
 #-------------------------------------------------------------------------------------------------------#
-print("Répertoire de Travail Actuel (CWD):", os.getcwd())
+
+# Data Cleaning and Preprocessing
 
 #Load all datasets csv
 df_mass_test = pd.read_csv('dataset/mass_case_description_test_set.csv')
@@ -26,24 +27,25 @@ df_combined = pd.concat([df_mass_test, df_mass_train,df_calc_train,df_calc_test]
 df_combined.head()
 print(df_combined.info())
 
+# use binary label for the type of disease
 df_combined['target'] = df_combined['pathology'].apply(
     lambda x: 1 if 'MALIGNANT' in str(x).upper() else 0
 )
 
-# 3. Colonnes Clés
+
 df_combined = df_combined[['patient_id', 'image file path', 'cropped image file path', 'target', 'abnormality id']]
 
+#create a copy
 df_combined_copy = df_combined.copy()
 
 print(f"DataFrame Maître créé. Nombre total d'images: {len(df_combined)}")
 print(f"Distribution des étiquettes:\n{df_combined['target'].value_counts()}")
 
-# Implémentation du Partitionnement avec "patient id"
+#Dataframe partition using "patient id"
 
-# --- 1. Identifier la pathologie dominante par patient ---
+# identify dominant pathology before spliting into groups of patients
 patients = df_combined.groupby('patient_id')['target'].agg(['mean', 'count']).reset_index()
 
-# 'is_malignant_dominant' est vrai si un patient a plus de 50% de cas Malins (1)
 patients['is_malignant_dominant'] = patients['mean'] > 0.5
 
 malignant_patients = patients[patients['is_malignant_dominant']]['patient_id'].tolist()
@@ -52,22 +54,23 @@ benign_patients = patients[~patients['is_malignant_dominant']]['patient_id'].tol
 print(f"Patients Malignant Dominant: {len(malignant_patients)}")
 print(f"Patients Benign Dominant: {len(benign_patients)}")
 
-# --- 2. Répartition Non-IID des patients ---
+#NON-IID repartition
 SEED_VALUE = 42
-np.random.seed(SEED_VALUE)  # Pour la reproductibilité
+np.random.seed(SEED_VALUE)
 
-# Client 1 (Majorité Malignant) : prend 70% des patients Malignant et 30% des Benign
+#Client-1 : Majority Malignant (70% vs 30%)
 N1_M = int(0.7 * len(malignant_patients))
 N1_B = int(0.3 * len(benign_patients))
 client1_M = np.random.choice(malignant_patients, N1_M, replace=False)
 client1_B = np.random.choice(benign_patients, N1_B, replace=False)
 client1_patients = np.concatenate([client1_M, client1_B])
 
-# Mise à jour des listes restantes
+# Update remaining lists
 malignant_patients_rest = [s for s in malignant_patients if s not in client1_M]
 benign_patients_rest = [s for s in benign_patients if s not in client1_B]
 
-# Client 2 (Majorité Benign) : prend 30% des Malignant restants et 70% des Benign restants
+
+#Client-2 : Majority Benign (70% vs 30%)
 N2_M = int(0.3 * len(malignant_patients_rest))
 N2_B = int(0.7 * len(benign_patients_rest))
 client2_M = np.random.choice(malignant_patients_rest, N2_M, replace=False)
@@ -75,11 +78,12 @@ client2_B = np.random.choice(benign_patients_rest, N2_B, replace=False)
 client2_patients = np.concatenate([client2_M, client2_B])
 
 # 3. Client 3 (Le reste)
+#Client-3 : rest of the patients
 all_patients = patients['patient_id'].tolist()
 used_patients = np.concatenate([client1_patients, client2_patients])
 client3_patients = [s for s in all_patients if s not in used_patients]
 
-# --- 4. Création et vérification des DataFrames Clients Finaux ---
+# Final dataframe
 client_dataframes = {}
 all_client_patients = {'client1': client1_patients, 'client2': client2_patients, 'client3': client3_patients}
 
--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 # Projet-AIfTWD
 Implementation of a functional minimal federated setup on Cancer Identification using medical imaging data 
-dataset : BreaKHis_v1
+
+dataset BreaKHis_v1 need to be downloaded and added to the folder "Partie1/dataset/" next to the csv files
+
 link to download the dataset : https://www.kaggle.com/datasets/awsaf49/cbis-ddsm-breast-cancer-image-dataset?resource=download
+
+first results :
+
+```text
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 3568 entries, 0 to 3567
+Data columns (total 17 columns):
+ #   Column                   Non-Null Count  Dtype  
+---  ------                   --------------  -----  
+ 0   patient_id               3568 non-null   object 
+ 1   breast_density           1696 non-null   float64
+ 2   left or right breast     3568 non-null   object 
+ 3   image view               3568 non-null   object 
+ 4   abnormality id           3568 non-null   int64  
+ 5   abnormality type         3568 non-null   object 
+ 6   mass shape               1692 non-null   object 
+ 7   mass margins             1636 non-null   object 
+ 8   assessment               3568 non-null   int64  
+ 9   pathology                3568 non-null   object 
+ 10  subtlety                 3568 non-null   int64  
+ 11  image file path          3568 non-null   object 
+ 12  cropped image file path  3568 non-null   object 
+ 13  ROI mask file path       3568 non-null   object 
+ 14  breast density           1872 non-null   float64
+ 15  calc type                1848 non-null   object 
+ 16  calc distribution        1433 non-null   object 
+dtypes: float64(2), int64(3), object(12)
+memory usage: 474.0+ KB
+None
+DataFrame Maître créé. Nombre total d'images: 3568
+Distribution des étiquettes:
+target
+0    2111
+1    1457
+Name: count, dtype: int64
+Patients Malignant Dominant: 710
+Patients Benign Dominant: 856
+
+--- Vérification de la Distribution Non-IID ---
+
+Client: client1
+Total images: 1622
+Distribution Malignant (1): 61.22%
+
+Client: client2
+Total images: 1187
+Distribution Malignant (1): 13.48%
+
+Client: client3
+Total images: 759
+Distribution Malignant (1): 40.05%
+
+Process finished with exit code 0
+```
